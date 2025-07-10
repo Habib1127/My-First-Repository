75c68acfb4bf52860cd85d54a4a35f61621fd806
Habib1127 <habibcse315@gmail.com>
2025-07-10 21:12:49 +0600

    Create OOP Class Problem.java
    Integer: 100
    Double: 99.99
    Character: A
    String: Java
    Boolean: True

---
+++ OOP Class Problem.java
@@ -0,0 +1,23 @@
+/*QUESION
+Integer: 100
+Double: 99.99
+Character: A
+String: Java
+Boolean: True*/
+
+public class Main
+{
+public static void main(String[] args)
+{
+int a= 100;
+double b= 99.99;
+char c= 'A';
+String d= "Java";
+boolean e= true;
+System.out.printf("Integer: %d\n",a);
+System.out.printf("Double: %.2f\n",b);
+System.out.printf("Character: %c\n",c);
+System.out.printf("String: %s\n",d);
+System.out.printf("Boolean: %b\n",e);
+}
+}
